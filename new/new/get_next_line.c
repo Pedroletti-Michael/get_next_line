f67cdcb11f4b115b054785d785ac338839aa2c81
Pedroletti Michael <pedrolettimichael@gmail.com>
2021-11-15 21:25:00 +0100

    les leaks mdr

--- new/new/get_next_line.c
+++ new/new/get_next_line.c
@@ -13,7 +13,7 @@ int	ft_strlen(const char *str)
 	return (i);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, void *src, size_t n)
 {
 	int				i;
 	char			*d;
@@ -32,7 +32,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, char *src, size_t size)
 {
 	unsigned int	len;
 
@@ -44,10 +44,11 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 		ft_memcpy(dest, src, size - 1);
 		dest[size - 1] = '\0';
 	}
+	//free(src);
 	return (len);
 }
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(char *src, int free_needed)
 {
 	char	*ptr;
 	int		len;
@@ -58,11 +59,13 @@ char	*ft_strdup(char *src)
 		ft_strlcpy(ptr, src, len + 1);
 	else
 		return (NULL);
+	if (free_needed == 1)
+		free(src);
 	return (ptr);
 }
 
 // Function modified, so this function may be the cause of some problems
-char	*ft_strjoin(char const *a, char const *b)
+char	*ft_strjoin(char *a, char *b)
 {
 	char	*r;
 	int		len;
@@ -78,6 +81,8 @@ char	*ft_strjoin(char const *a, char const *b)
 		ft_strlcpy(r, a, ft_strlen(a) + 1);
 		ft_strlcpy(r + ft_strlen(a), b, ft_strlen(b) + 1);
 		//printf("r is %c (%i)\n", r[2], (int)r[2]);
+		free(a);
+		//free(b);
 		return (r);
 	}
 	else
@@ -85,7 +90,7 @@ char	*ft_strjoin(char const *a, char const *b)
 }
 
 /* This function's going to check if a new line is already on the buffer and if it's true, that's going to return the new line and stock the rest on the static var buf */
-char	*chck_nl(char **buf)
+char	*chck_nl(char **buf, int endfile)
 {
 	char	*res;
 	int		i;
@@ -100,6 +105,8 @@ char	*chck_nl(char **buf)
 			j = i;
 		i++;
 	}
+	if (endfile)
+		j = i;
 	if (j == -42)
 		return (NULL);
 	res = malloc(sizeof(char) * (j + 1));
@@ -112,26 +119,42 @@ char	*chck_nl(char **buf)
 		i++;
 	}
 	res[i] = '\0';
-	*buf = ft_strdup(*buf + (j + 1));
+	*buf = ft_strdup(*buf + (j + 1), 1);
 	return (res);
 }
 
+int	chck_endfile(char **buf)
+{
+	int	i;
+
+	i = 0;
+	while (i <= BUFFER_SIZE)
+	{
+		if ((*buf)[i] == '\0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 /* This function's going to read a file while no new line is on the buffer, return the new line and add the surplus into the static var buf */
-char	*rd_file(char **buf, int fd)
+char	*rd_file(char **buf, int fd, int endfile)
 {
 	char	*res;
 	char	*tmp;
 
-	tmp = ft_strdup(*buf);
+	tmp = ft_strdup(*buf, 0);
 	while (read(fd, *buf, BUFFER_SIZE) > 0)
 	{
+		if (chck_endfile(buf))
+			endfile = 1;
 		//Verifier si le buf contient un \0 si c'est le cas faire une action special
 		tmp = ft_strjoin(tmp, *buf);
 		//printf("tmp is %c (%i)\n", tmp[2], (int)tmp[2]);
-		res = chck_nl(&tmp);
+		res = chck_nl(&tmp, endfile);
 		if (res)
 		{
-			*buf = ft_strdup(tmp);
+			*buf = ft_strdup(tmp, 0);
 			free(tmp);
 			return (res);
 		}
@@ -143,23 +166,30 @@ char	*rd_file(char **buf, int fd)
 char	*get_next_line(int fd)
 {
 	static char	*buf;
+	static int	endfile;
 	char		*res;
 
 	if (buf)
 	{
-		res = chck_nl(&buf);
+		res = chck_nl(&buf, endfile);
 		if (!res)
 		{
-			res = rd_file(&buf, fd);
+			res = rd_file(&buf, fd, endfile);
 			if (!buf)
 				free(buf);
 		}
 	}
 	else
 	{
+		endfile = 0;
 		buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		res = rd_file(&buf, fd);
+		res = rd_file(&buf, fd, endfile);
 	}
+	if (endfile && !res)
+	{
+		free(buf);
+	}
+
 	return (res);
 }
 
@@ -167,8 +197,9 @@ char	*get_next_line(int fd)
 {
 	int	fd;
 
-	fd = open("41_no_nl", 0);
+	fd = open("41_nos_nl", 0);
 	printf("1. /%s\\\n", get_next_line(fd));
 	printf("2. /%s\\\n", get_next_line(fd));
 	close(fd);
+	return (NULL);
 }*/
